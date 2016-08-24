d748ce5245da4012924a19f31a0510d5d39d1436
tttimit <timit.cloud@gmail.com>
2016-08-24 22:20:55 +0800

    directory change

--- others/py0.py
+++
@@ -1,7 +0,0 @@
-# Problem Set 0
-# Name: Tim Cheung
-# Collaborators: nobody
-# Time Spent: 2:00
-birth = input("Enter your date of birth: \n")
-name = input("Enter your last name: \n")
-print(name, birth)
--- others/t.py
+++
@@ -1,5 +0,0 @@
-x = int(input('enter an integer: '))
-if x == 5:
-    print('you input 5')
-else:
-    print('you didn\'t input 5')
