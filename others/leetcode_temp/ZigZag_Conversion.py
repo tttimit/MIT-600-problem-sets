e6bb9569b07740a4873fa94b736ca4970a5805e7
tttimit <timit.cloud@gmail.com>
2016-08-25 18:05:26 +0800

    添加遗失的文件

---
+++ others/leetcode_temp/ZigZag_Conversion.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1: return s
+        elif numRows == 2:
+            
+            
+
+
+                                             
+s = Solution()
+
+def test(str, numRows):
+    print(s.convert(str, numRows))
+
+##test('ABC', 2)
+##test('ABC', 1)
+##test('ABCD', 2)
+##test('A', 1)
+test('PAYPALI', 3)
+test('PAYLALISHIRING', 3)
